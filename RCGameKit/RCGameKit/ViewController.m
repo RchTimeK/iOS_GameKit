1f4b73434ec83e0d1c6c1cd1d23ee2f211edbe3e
Jone Zhang <NSObject_RC@163.com>
2016-07-28 17:17:53 +0800

    初始化项目

---
+++ RCGameKit/RCGameKit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RCGameKit
+//
+//  Created by RongCheng on 16/7/28.
+//  Copyright © 2016年 RongCheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"----------------");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
